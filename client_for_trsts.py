04293084f5d114ce3ec8adf3bf64efc9d1e65c58
Burdeinick <burdeinick@yandex.ru>
2020-05-31 23:01:33 +0300

    The first change

---
+++ client_for_trsts.py
@@ -0,0 +1,94 @@
+import requests
+
+def get_users():
+    """ For testing geting all users"""
+    url = r'http://127.0.0.1:5000/api/v1/user/list'
+    headers = {"UserName": "Bob", "UserSecret": "1234"}
+    requests.post(url, headers=headers)
+
+def board_create():
+    """For board create"""
+    url = r'http://127.0.0.1:5000/api/v1/board/create'
+    headers = {"UserName": "Bob", "UserSecret": "1234"}
+    data = {
+        "title": "Доска разработчика",
+        "columns": [
+                    "ToDo",
+                    "InProgress",
+                    "Done"
+                    ]
+            }
+    requests.post(url, json=data, headers=headers)
+
+def board_delete():
+    """ For board delete"""
+    url = r'http://127.0.0.1:5000/api/v1/board/delete'
+    headers = {"UserName": "Bob", "UserSecret": "1234"}
+    data = {"title": "Доска разработчика"}
+    requests.post(url, json=data, headers=headers)
+
+def board_list():
+    """ Get all boards """
+    url = r'http://127.0.0.1:5000/api/v1/board/list'
+    headers = {"UserName": "Bob", "UserSecret": "1234"}
+    requests.post(url, headers=headers)   
+
+def card_create():
+
+    url = r'http://127.0.0.1:5000/api/v1/card/create'
+    headers = {"UserName": "Bob", "UserSecret": "1234"}
+    data = {
+            "title": "Развернуть PostgreSQL",
+            "board": "Доска разработчика",
+            "status": "ToDo",
+            "description": "Необходимо развернуть базу данных PostgreSQL",
+            "assignee": "Username",
+            "estimation": "8h"
+            }
+    requests.post(url, json=data, headers=headers) 
+
+def card_update():
+    """ For update a card"""
+    url = r'http://127.0.0.1:5000/api/v1/card/update'
+    headers = {"UserName": "Bob", "UserSecret": "1234"}
+    data = {
+            "title": "Развернуть PostgreSQL",
+            "board": "Доска разработчика",
+            "status": "Done"
+            }
+    requests.post(url, json=data, headers=headers) 
+
+def card_delete():
+    """ For delete a card"""
+    url = r'http://127.0.0.1:5000/api/v1/card/delete'
+    headers = {"UserName": "Bob", "UserSecret": "1234"}
+    data = {
+            "title": "Развернуть PostgreSQL",
+            "board": "Доска разработчика"
+            }
+
+    requests.post(url, json=data, headers=headers) 
+
+def colum_info():
+    """ Get info about a task"""
+    url = r'http://127.0.0.1:5000/api/v1/report/cards_by_column'
+    headers = {"UserName": "Bob", "UserSecret": "1234"}
+    data = {
+            "board": "Доска разработчика",
+            "column": "ToDo",
+            "assignee": "Username"
+            }
+    requests.post(url, json=data, headers=headers) 
+
+# get_users()
+# board_create()
+# board_delete()
+# board_list()
+# card_create()
+# card_update()
+# card_delete()
+# colum_info()
+
+
+
+
---
+++ server.py
@@ -0,0 +1,58 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+
+@app.route("/api/v1/user/list", methods=['POST'])
+def get_all_users():
+    """ Get all users"""
+    print('Запрос пришел, все в норме')
+    print(request.headers)
+    return 'Запрос пришел, все в норме'
+
+@app.route("/api/v1/board/create", methods=['POST'])
+def board_create():
+    """ For create a snew board"""
+    print('Я буду создавать доски')
+    print(request.headers)
+    print(request.json)
+    return 'Ok'
+
+@app.route("/api/v1/board/delete", methods=['POST'])
+def board_delete():
+    """ For delete board"""
+    print(request.json)
+    return 'Ok'
+
+@app.route("/api/v1/board/list", methods=['POST'])
+def board_list():
+    """ Get all boards"""
+    print(request.headers)
+    return 'ok'
+
+@app.route("/api/v1/card/create", methods=['POST'])
+def card_create():
+    """ For cread a new card"""
+    print(request.json)
+    return 'Ok'
+
+@app.route("/api/v1/card/update", methods=['POST'])
+def card_update():
+    """ For update a card"""
+    print(request.json)
+    return 'Ok'
+
+@app.route("/api/v1/card/delete", methods=['POST'])
+def card_delete():
+    """ For delete a card"""
+    print(request.json)
+    return 'Ok'
+
+@app.route("/api/v1/report/cards_by_column", methods=['POST'])
+def colum_info():
+    """ Get info about a task"""
+    print(request.json)
+    return 'Ok'
+
+
+if __name__ == "__main__":
+    app.run()
