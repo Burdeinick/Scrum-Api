f907e9f04ea3ccd41665cbde2a915b9d56ff4315
Бурдейник Александр Витальевич <burdeinick@yandex.ru>
2020-06-05 13:44:57 +0300

    05.06.20

--- logic.py
+++ logic.py
@@ -7,45 +7,76 @@ user = "alex"
 password = "0525"
 
 
+class Statuses:
+    """ """
+    def __init__(self):
+        self.such_board_exists = {"status": "This a board already exist"}
+        self.new_board_create = {"status": "The board was created"}
+        self.new_board_dont_create = {"status": "The new board was don't created"}
+        self.authentif_error = {"Authentification": "Error"}
+
 class ConnectionDB:
     """ """
     def __init__(self):
-        self.conn = psycopg2.connect(dbname=dbname, user=user, password=password)
+        self.conn = psycopg2.connect(dbname=dbname,
+                                     user=user,
+                                     password=password)
         self.cursor = self.conn.cursor()
 
 
 class RequestsDB:
     """ """
     def __init__(self):
-        self.connect_db = ConnectionDB().cursor
-        self.conn = ConnectionDB().conn
+        self.connect_db = ConnectionDB()
+        self.statuses = Statuses()
 
     def request_get_all_users(self) -> list:
         """ """
-        self.connect_db.execute("SELECT username, password FROM users")
-        return self.connect_db.fetchall()
+        self.connect_db.cursor.execute("SELECT username, password   \
+                                        FROM users")
+        return self.connect_db.cursor.fetchall()
 
     def request_create_new_board(self, collecte_data: tuple):
+        """ The function for requests to DB for adding new board in "boards" table. """
+        title = collecte_data[0]
+        columns = collecte_data[1]
+        created_at = collecte_data[2]
+        created_by = collecte_data[3]
+        last_updated_at = collecte_data[4]
+        last_updated_by = collecte_data[5]
 
+        request = f"INSERT INTO boards(                     \
+                                        title,              \
+                                        columns,            \
+                                        created_at,         \
+                                        created_by,         \
+                                        last_updated_at,    \
+                                        last_updated_by     \
+                                        )                   \
+                    values(                       \
+                            '{title}',            \
+                            '{columns}',          \
+                            '{created_at}',       \
+                            '{created_by}',       \
+                            '{last_updated_at}',  \
+                            '{last_updated_by}'   \
+                            )"
 
+        self.connect_db.cursor.execute(request)                                       
+        self.connect_db.conn.commit()
 
-        self.connect_db.execute(f"INSERT INTO boards(title, \
-                                                    columns, \
-                                                    created_at, \
-                                                    created_by, \
-                                                    last_updated_at, \
-                                                    last_updated_by)  \
-                                  values('{collecte_data[0]}', \
-                                         '{collecte_data[1]}', \
-                                         '{collecte_data[2]}', \
-                                         '{collecte_data[3]}', \
-                                         '{collecte_data[4]}', \
-                                         '{collecte_data[5]}')")
+        if self.connect_db.cursor.statusmessage == "INSERT 0 1":
+            return self.statuses.new_board_create
+        return self.statuses.new_board_dont_create
 
-        self.conn.commit()
-        print(self.connect_db.statusmessage)
+    def request_check_board_avail(self):
+        request = f"SELECT title    \
+                    FROM boards"
 
-        # self.connect_db.execute("SELECT * FROM boards")
+        self.connect_db.cursor.execute(request)                                       
+        self.connect_db.conn.commit()
+
+        return self.connect_db.cursor.fetchall()
 
 
 class UsingDB:
@@ -53,6 +84,7 @@ class UsingDB:
     def __init__(self, data=None):
         self.data = data
         self.req_DB = RequestsDB()
+        self.statuses = Statuses()
 
     def autefication_users(self, name_secret: tuple) -> bool:
         """ For authentification of the users  """
@@ -60,7 +92,7 @@ class UsingDB:
         usersecret = name_secret[1]
         
         respons_of_DB = self.req_DB.request_get_all_users()
-        print('Я в autefication_users', respons_of_DB)
+        # print('Я в autefication_users', respons_of_DB)
         for step in respons_of_DB:
             if username == step[0]:
                 if usersecret == step[1]:
@@ -71,10 +103,7 @@ class UsingDB:
     def get_all_users(self) -> dict:
         """ This function can get all the users from DB """
         # !!!!!!!!!!!!!!!!!!!!!!!! TOdo кол-во досок!!!!!!!!
-        respons_to_serv = {
-                            "count": '',
-                            "users": []
-                            }
+        respons_to_serv = {"users": []}
 
         respons_of_DB = self.req_DB.request_get_all_users()
 
@@ -82,12 +111,8 @@ class UsingDB:
             respons_to_serv["users"].append({"username": step[0]})
         return respons_to_serv
 
-
-
-
     def create_new_board(self, data: dict, username: str):
         """ For create a new board """
-
         title = data["title"]
         colums = ' '.join(data["columns"])
         created_at = time.time()
@@ -104,5 +129,19 @@ class UsingDB:
                          last_updated_by
                         )
 
-        self.req_DB.request_create_new_board(collecte_data)
+
+        respons_of_DB_same_title = self.req_DB.request_check_board_avail()  # Проверка на наличие такой доски в БД
+        print()
+
+        for title_in_DB in respons_of_DB_same_title:
+            if title_in_DB[0] ==  title:
+                print('1', 'Доска не создалась!', {"status": "This a board already exist"})
+                return self.statuses.such_board_exists
+
+            if self.req_DB.request_create_new_board(collecte_data):
+                print('2', {"status": "The board was created"})
+                return self.statuses.such_board_exists
+
+        print('3', {"status": "The new board was don't created"})
+        return self.statuses.new_board_dont_create
         
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from logic import UsingDB
+from logic import Statuses
 import psycopg2
 app = Flask(__name__)
 
@@ -19,21 +20,20 @@ def get_all_users():
     else:
         print({'Authentification': 'Error'})
         return {'Authentification': 'Error'}
+
+
+
+
+
+
+
+
+
+
+
+
+
         
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 @app.route("/api/v1/board/create", methods=['POST'])
 def board_create():
     """ For create a snew board"""
@@ -42,19 +42,26 @@ def board_create():
     autenf_data = (username, usersecret)
     data = request.json
     us_db = UsingDB()
+    statuses = Statuses()
 
     if us_db.autefication_users(autenf_data):
         print('Аутенфикация пройдена')
-        us_db.create_new_board(data, username)
-        return({'Status': 'A board was created'})
+
+        if (us_db.create_new_board(data, username)) == statuses.new_board_create:
+            print('Serv 1', {"status": "The board was created"})
+            return statuses.such_board_exists
+
+        if (us_db.create_new_board(data, username)) == statuses.such_board_exists:
+            print('Serv 2', {"status": "This a board already exist"})
+            return statuses.such_board_exists
+
+        if (us_db.create_new_board(data, username)) == statuses.new_board_dont_create :   
+            print('Serv 3', {"status": "The new board was don't created"})
+            return statuses.new_board_dont_create
+
     else:
         print({'Authentification': 'Error'})
-        return {'Authentification': 'Error'}
-
-
-
-
-
+        return statuses
 
 
 
@@ -78,6 +85,27 @@ def board_delete():
     print(request.json)
     return 'Ok'
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @app.route("/api/v1/board/list", methods=['POST'])
 def board_list():
     """ Get all boards"""
@@ -93,13 +121,13 @@ def card_create():
 @app.route("/api/v1/card/update", methods=['POST'])
 def card_update():
     """ For update a card"""
-    print(request.json)
+    print( request.json)
     return 'Ok'
 
 @app.route("/api/v1/card/delete", methods=['POST'])
 def card_delete():
     """ For delete a card"""
-    print(request.json)
+    print( request.json)
     return 'Ok'
 
 @app.route("/api/v1/report/cards_by_column", methods=['POST'])
